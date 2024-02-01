c62a1b765be457b0a49f8a56e8f0a5d2cc9882d3
IamHuadong <1761339618@qq.com>
2024-02-01 22:47:41 +0800

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { TransformInterceptor } from 'src/interceptors/transform.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
 
   const config = new DocumentBuilder()
     .setTitle('API Document')
